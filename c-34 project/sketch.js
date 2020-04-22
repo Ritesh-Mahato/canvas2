e084b8031e445769a1677e8316b5ce429406f832
Ritesh-Mahato <60291946+Ritesh-Mahato@users.noreply.github.com>
2020-04-22 22:23:12 +0530

    Add files via upload

---
+++ c-34 project/sketch.js
@@ -0,0 +1,30 @@
+var database,canvas;
+var drawing=[];
+function setup(){
+    canvas= createCanvas(400,400);
+    database=firebase.database();
+}
+
+function draw(){
+    background(250);
+
+    if(mouseIsPressed){
+        var point = {
+            x:mouseX,
+            y:mouseY
+        }
+        drawing.push(point); 
+    }
+
+    //console.log(drawing);
+
+    beginShape();
+    stroke(0);
+    strokeWeight(4);
+    noFill();
+    for(var i=0; i<drawing.length;i++){
+        vertex(drawing[i].x,drawing[i].y)
+    }
+    endShape();
+
+}
